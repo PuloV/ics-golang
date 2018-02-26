946be6939e7866646b9f22ca5069c3a6654eceac
Robert Horn <hornrj@robs.office>
2018-02-26 13:04:23 -0500

    Allow presence of optional and other defined parameters.  These are ignored, but no longer interfere with extracting the parameters of interest.

--- parse.go
+++ parse.go
@@ -431,9 +431,9 @@ func (p *Parser) parseEvents(cal *Calendar, eventsData []string) {
 
 // parses the event summary
 func (p *Parser) parseEventSummary(eventData string) string {
-	re, _ := regexp.Compile(`SUMMARY:.*?\n(?:\s+.*?\n)*`)
+	re, _ := regexp.Compile(`SUMMARY(?:;.*?|):.*?\n(?:\s+.*?\n)*`)  // This will accept parameters and additional lines
 	result := re.FindString(eventData)
-	return trimField(strings.Replace(result, "\r\n ", "", -1), "SUMMARY:")
+	return trimField(strings.Replace(result, "\r\n ", "", -1), "SUMMARY.*?:")  // This concatenates additional lines and discards all parameters
 }
 
 // parses the event status
@@ -445,47 +445,47 @@ func (p *Parser) parseEventStatus(eventData string) string {
 
 // parses the event description
 func (p *Parser) parseEventDescription(eventData string) string {
-	re, _ := regexp.Compile(`DESCRIPTION:.*?\n(?:\s+.*?\n)*`)
+	re, _ := regexp.Compile(`DESCRIPTION(?:;.*?|):.*?\n(?:\s+.*?\n)*`)  // This will accept parameters and additional lines
 	result := re.FindString(eventData)
-	return trimField(strings.Replace(result, "\r\n ", "", -1), "DESCRIPTION:")
+	return trimField(strings.Replace(result, "\r\n ", "", -1), "DESCRIPTION.*?:")  // This concatenates additional lines and discards all parameters
 }
 
 // parses the event id provided form google
 func (p *Parser) parseEventId(eventData string) string {
-	re, _ := regexp.Compile(`UID:.*?\n`)
+	re, _ := regexp.Compile(`UID;?.*?:.*?\n`)  // This will accept parameters
 	result := re.FindString(eventData)
-	return trimField(result, "UID:")
+	return trimField(result, "UID.*?:")  // This will discard all parameters
 }
 
 // parses the event class
 func (p *Parser) parseEventClass(eventData string) string {
-	re, _ := regexp.Compile(`CLASS:.*?\n`)
+	re, _ := regexp.Compile(`CLASS;?.*?:.*?\n`) // This will accept parameters
 	result := re.FindString(eventData)
-	return trimField(result, "CLASS:")
+	return trimField(result, "CLASS.*?:")  // This will discard all parameters
 }
 
 // parses the event sequence
 func (p *Parser) parseEventSequence(eventData string) int {
-	re, _ := regexp.Compile(`SEQUENCE:.*?\n`)
+	re, _ := regexp.Compile(`SEQUENCE;?.*?:.*?\n`) // This will accept parameters
 	result := re.FindString(eventData)
-	sq, _ := strconv.Atoi(trimField(result, "SEQUENCE:"))
+	sq, _ := strconv.Atoi(trimField(result, "SEQUENCE.*?:"))  // This will discard all parameters
 	return sq
 }
 
 // parses the event created time
 func (p *Parser) parseEventCreated(eventData string) time.Time {
-	re, _ := regexp.Compile(`CREATED:.*?\n`)
+	re, _ := regexp.Compile(`CREATED;?.*?:.*?\n`)  // This will accept parameters
 	result := re.FindString(eventData)
-	created := trimField(result, "CREATED:")
+	created := trimField(result, "CREATED.*?:")  // This will discard all parameters
 	t, _ := time.Parse(IcsFormat, created)
 	return t
 }
 
 // parses the event modified time
 func (p *Parser) parseEventModified(eventData string) time.Time {
-	re, _ := regexp.Compile(`LAST-MODIFIED:.*?\n`)
+	re, _ := regexp.Compile(`LAST-MODIFIED;?.*?:.*?\n`)  // This will accept parameters
 	result := re.FindString(eventData)
-	modified := trimField(result, "LAST-MODIFIED:")
+	modified := trimField(result, "LAST-MODIFIED.*?:")  // This will discard all parameters
 	t, _ := time.Parse(IcsFormat, modified)
 	return t
 }
@@ -543,24 +543,24 @@ func (p *Parser) parseEventEnd(eventData string) time.Time {
 
 // parses the event RRULE (the repeater)
 func (p *Parser) parseEventRRule(eventData string) string {
-	re, _ := regexp.Compile(`RRULE:.*?\n`)
+	re, _ := regexp.Compile(`RRULE;?.*?:.*?\n`)  // This will accept parameters
 	result := re.FindString(eventData)
-	return trimField(result, "RRULE:")
+	return trimField(result, "RRULE.*?:")  // This will discard all parameters
 }
 
 // parses the event LOCATION
 func (p *Parser) parseEventLocation(eventData string) string {
-	re, _ := regexp.Compile(`LOCATION:.*?\n`)
+	re, _ := regexp.Compile(`LOCATION(?:;.*?|):.*?\n(?:\s+.*?\n)*`) // This will accept parameters and additional lines
 	result := re.FindString(eventData)
-	return trimField(result, "LOCATION:")
+	return trimField(strings.Replace(result, "\r\n ", "", -1), "LOCATION.*?:") // This concatenates additional lines and discards all parameters
 }
 
 // parses the event GEO
 func (p *Parser) parseEventGeo(eventData string) *Geo {
-	re, _ := regexp.Compile(`GEO:.*?\n`)
+	re, _ := regexp.Compile(`GEO;?.*?:.*?\n`)  // This will accept parameters
 	result := re.FindString(eventData)
 
-	value := trimField(result, "GEO:")
+	value := trimField(result, "GEO.*?:")  // This will discard all parameters
 	values := strings.Split(value, ";")
 	if len(values) < 2 {
 		return nil
